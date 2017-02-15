4988052b5881449444c43b0c39d0806e41a2a3f6
Rob Earwaker <rob.earwaker@gmail.com>
2017-02-15 22:19:43 +0000

    Improve test case

--- test_rail.py
+++ test_rail.py
@@ -27,12 +27,14 @@ class TestMatch(unittest.TestCase):
 
     def test_value_unmatched_by_all_match_statements(self):
         with self.assertRaises(rail.UnmatchedValueFailure) as context:
+            value = mock.Mock()
             rail.match(
                 (lambda value: value == 'mock', lambda value: mock.Mock()),
-                (lambda value: len(value) > 9, lambda value: mock.Mock()),
+                (lambda value: value is None, lambda value: mock.Mock()),
+                (lambda value: False, lambda value: mock.Mock()),
                 (lambda value: type(value) == float, lambda value: mock.Mock())
-            )('value')
-        self.assertEqual('value', context.exception.value)
+            )(value)
+        self.assertEqual(value, context.exception.value)
 
 
 if __name__ == '__main__':
