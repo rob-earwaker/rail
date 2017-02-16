2f0c1af7d66e23c2805d3bc88506f9701208cfd7
Rob Earwaker <rob.earwaker@gmail.com>
2017-02-16 19:42:23 +0000

    Simplified match failure tests

--- test_rail.py
+++ test_rail.py
@@ -20,19 +20,16 @@ class TestRaiseException(unittest.TestCase):
 class TestMatch(unittest.TestCase):
     def test_no_match_statements_provided(self):
         with self.assertRaises(rail.UnmatchedValueFailure) as context:
-            rail.match(
-                (lambda value: value == 'mock', lambda value: mock())
-            )('value')
+            rail.match()('value')
         self.assertEqual('value', context.exception.value)
 
     def test_value_unmatched_by_all_match_statements(self):
         with self.assertRaises(rail.UnmatchedValueFailure) as context:
             value = mock.Mock()
             rail.match(
-                (lambda value: value == 'mock', lambda value: mock.Mock()),
-                (lambda value: value is None, lambda value: mock.Mock()),
                 (lambda value: False, lambda value: mock.Mock()),
-                (lambda value: type(value) == float, lambda value: mock.Mock())
+                (lambda value: False, lambda value: mock.Mock()),
+                (lambda value: False, lambda value: mock.Mock())
             )(value)
         self.assertEqual(value, context.exception.value)
 
